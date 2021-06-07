c0c865afdf32a59926b5ac63ef7c5a474978fe8e
Victor Uriarte <uriarte0505@gmail.com>
2021-06-07 11:02:33 -0300

    First Commit; Creating custom textfield

---
+++ window-shopper/Controller/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  window-shopper
+//
+//  Created by Victor Uriarte on 07/06/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,23 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Victor Uriarte on 07/06/21.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.254249017)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+}
